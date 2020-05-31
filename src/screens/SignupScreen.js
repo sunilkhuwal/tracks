b3ff302374d016474bda8bd156237dac873249c2
khuwals <sunil.khuwal@gmail.com>
2020-05-31 18:52:11 +0530

    data context one of these is AuthContext

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
       <Spacer />
-      <Input label="Username" />
+      <Input
+        label="Username"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={username}
+        onChangeText={setUserName}      //Another way -> onChangeText={(username) => setUserName(username)}
+      />
 
-      <Input label="Password" />
+      <Input
+        label="Password"
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+      />
       <Spacer>
         <Button title="Sign Up" />
       </Spacer>
